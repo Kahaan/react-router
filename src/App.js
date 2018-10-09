09a5cc2417d8d92b971b1e4a5bb7093b8820f615
Kahaan <kahaan.r.patel@gmail.com>
2018-10-09 13:17:34 -0700

    reactRouter

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
-import {BrowserRouter, Route} from 'react-router-dom'
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import Home from './home'
 import Contact from './contact'
 import About from './about'
+import Navigation from './navigation'
+import Error from './error'
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div>
-          <Route exact path="/" component={Home} />
-          <Route path="/contact" component={Contact} />
-          <Route path="/about" component={About} />
+          <Navigation />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/about" component={About} />
+            <Route component={Error} />
+          </Switch>
         </div>
       </BrowserRouter>
     );
---
+++ src/error.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Error = (props) => {
+  return (
+    <div>
+      <p>Error</p>
+    </div>
+  )
+}
+
+export default Error
