4afa0cb826d35f668a6144756d815205174fcb51
Kahaan <kahaan.r.patel@gmail.com>
2018-10-09 12:46:36 -0700

    reactRouter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import {BrowserRouter, Route} from 'react-router-dom'
 import Home from './home'
+import Contact from './contact'
+import About from './about'
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-
+        <div>
+          <Route exact path="/" component={Home} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/about" component={About} />
+        </div>
       </BrowserRouter>
     );
   }
